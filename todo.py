5cd28deaf1f2d58e708cf3e82d199d1134b2310b
AndreyKarev <mr.andkar@vk.com>
2022-06-16 16:11:33 +0300

    add save() function

--- todo.py
+++ todo.py
@@ -2,6 +2,12 @@ import pickle
 import datetime
 import os
 
+
+def save():
+    f = open(todofile, 'wb')
+    pickle.dump(todo, f)
+    f.close()
+
 # Creating and loading a dictionary
 todofile = 'todo.data'
 if not os.path.isfile(todofile):
@@ -9,6 +15,7 @@ if not os.path.isfile(todofile):
     f = open(todofile, 'wb')
     pickle.dump(todo, f)
     f.close()
+    save()
 f = open(todofile, 'rb')
 todo = pickle.load(f)
 f.close()
@@ -16,13 +23,13 @@ f.close()
 # greetings
 print('''
 
-                Hi! I'm Casi, your virtual assistant. 
+                Hi! I'm Casi, your virtual assistant.
     So far, I'm not very developed, so I live here on the to-do list.
     But I promise that someday my developer, Andy, will make me
-                        MUCH BETTER! 
-                
+                        MUCH BETTER!
+
                 Let me tall you more about my functions:
-                
+
             1) View current to-do list (today)
             2) View cases for a specific date (date)
             3) Adding a task for today (add)
@@ -32,20 +39,20 @@ print('''
             7) Exit (exit)
 
                         Productive work!
-                        
+
                             :        :
                             |\______/|
                             | 0 _ 0  |
 ''')
 
 # start
-tsk = input("How can I help? ")
+task = input("How can I help? ")
 
 # The program runs until the user types "exit"
-while tsk != "exit":
+while task != "exit":
 
     # function 1
-    if tsk == "today":
+    if task == "today":
         day = str(datetime.date.today())
         if day in todo and todo[day] != []:
             print("Here is the list of tasks for today: ")
@@ -53,10 +60,10 @@ while tsk != "exit":
                 print(" - {0}".format(task))
         else:
             print("No tasks for today :)")
-        tsk = input("How else I can help? ")
+        task = input("How else I can help? ")
 
     # function 2
-    elif tsk == 'date':
+    elif task == 'date':
         day = input("Enter the date in the format yyyy-mm-dd: ")
         if not todo[day]:
             print("No tasks for this day :)")
@@ -64,41 +71,35 @@ while tsk != "exit":
             print("Here is the list of tasks for this day: ")
             for task in todo[day]:
                 print(" - {0}".format(task))
-        tsk = input("How else I can help? ")
+        task = input("How else I can help? ")
 
     # function 3
-    elif tsk == "add":
+    elif task == "add":
         task = input("What needs to be done? ")
         day = str(datetime.date.today())
         todo[day].append(task)
-        f = open(todofile, 'wb')
-        pickle.dump(todo, f)
-        f.close()
-        tsk = input("The task is listed. How else I can help? ")
+        save()
+        task = input("The task is listed. How else I can help? ")
 
     # function 4
-    elif tsk == 'add_d':
+    elif task == 'add_d':
         day = input("Enter the date in the format yyyy-mm-dd: ")
         if day != str(datetime.date.today()):
             todo[day] = []
         task = input("What needs to be done? ")
         todo[day].append(task)
-        f = open(todofile, 'wb')
-        pickle.dump(todo, f)
-        f.close()
-        tsk = input("The task is listed. How else I can help? ")
+        save()
+        task = input("The task is listed. How else I can help? ")
 
     # function 5
-    elif tsk == "del":
+    elif task == "del":
         trash = input("Enter the number of the task to be deleted,\nor press 'enter' to completely clear the list. ")
         day = str(datetime.date.today())
         if todo[day]:
             if not trash:
                 for i in range(len(todo[day])):
                     del todo[day][i]
-                    f = open(todofile, 'wb')
-                    pickle.dump(todo, f)
-                    f.close()
+                    save()
                     print("List cleared successfully. ")
             else:
                 try:
@@ -106,22 +107,18 @@ while tsk != "exit":
                     del todo[day][int(trash) - 1]
                 except:
                     print("Tasks with this number are not in the list.")
-                f = open(todofile, 'wb')
-                pickle.dump(todo, f)
-                f.close()
-        tsk = input("How else I can help? ")
+                save()
+        task = input("How else I can help? ")
 
     # function 6
-    elif tsk == "del_d":
+    elif task == "del_d":
         day = input("Enter the date in the format yyyy-mm-dd: ")
         trash = input("Enter the number of the task to be deleted,\nor press 'enter' to completely clear the list. ")
         if todo[day]:
             if not trash:
                 for i in range(len(todo[day])):
                     del todo[day][i]
-                f = open(todofile, 'wb')
-                pickle.dump(todo, f)
-                f.close()
+                save()
                 print("List cleared successfully. ")
             else:
                 try:
@@ -129,13 +126,11 @@ while tsk != "exit":
                     del todo[day][int(trash) - 1]
                 except:
                     print("Tasks with this number are not in the list.")
-                f = open(todofile, 'wb')
-                pickle.dump(todo, f)
-                f.close()
-        tsk = input("How else I can help? ")
+                save()
+        task = input("How else I can help? ")
 
     # In case of an unexpected command
     else:
         print("Alas, I can't handle it :(")
-        tsk = input("How else I can help? ")
+        task = input("How else I can help? ")
 print("It was a pleasure to work with you ;)")
